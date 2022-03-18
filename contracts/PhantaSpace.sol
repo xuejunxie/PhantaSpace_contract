308a1f75d62acd3d121d4183ec172fb3755f7564
xuejun xie <Xuejun Xie>
2022-03-17 20:55:58 -0700

    read and write contract working

--- contracts/PhantaSpace.sol
+++ contracts/PhantaSpace.sol
@@ -1,6 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+
+//   _____  _                 _         _____                      
+//  |  __ \| |               | |       / ____|                     
+//  | |__) | |__   __ _ _ __ | |_ __ _| (___  _ __   __ _  ___ ___ 
+//  |  ___/| '_ \ / _` | '_ \| __/ _` |\___ \| '_ \ / _` |/ __/ _ \
+//  | |    | | | | (_| | | | | || (_| |____) | |_) | (_| | (_|  __/
+//  |_|    |_| |_|\__,_|_| |_|\__\__,_|_____/| .__/ \__,_|\___\___|
+//                                           | |                   
+//                                           |_|                   
+
+
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
