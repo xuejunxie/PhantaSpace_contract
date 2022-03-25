99c5c5fc17c96ffc1cd118f1c9ba3185c42fed89
xuejun xie <Xuejun Xie>
2022-03-24 22:45:36 -0700

    some update on testing and contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,8 +7,6 @@ require("@openzeppelin/hardhat-upgrades");
 require("hardhat-gas-reporter");
 require("dotenv").config();
 
-const { mnemonic, moralisApiKey } = require("./secrets.json");
-
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -27,7 +25,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: {
-    version: "0.8.4",
+    version: "0.8.13",
     settings: {
       optimizer: {
         enabled: true,
